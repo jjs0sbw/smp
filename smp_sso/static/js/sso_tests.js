2363b64f2397bf44d840bccafbb937e97192e92b
Joseph Simpson <jjs0sbw@gmail.com>
2014-04-09 06:50:47 -0700

    Creating baseline unit tests

--- smp_sso/static/js/sso_tests.js
+++ smp_sso/static/js/sso_tests.js
@@ -1,3 +1,9 @@
+// First standard test
 test("First test", function() {
-ok( 1 == "1", "Passed!");
+	ok( 1 == "1", "Passed!");
 });
+
+test("SSO_binary_math.available", function() {
+	ok( SSO.notice == "Special binary matrix and vector type.", "SSO notice is not here");	
+});
+
